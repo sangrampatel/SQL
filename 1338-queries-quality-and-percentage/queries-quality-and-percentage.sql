7b8c375041aeaa1ddaca519996e2d5c236db2bec
sangrampatel <sangrampatel2004@gmail.com>
2026-07-29 20:47:29 +0530

    Time: 470 ms (11.41%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select  query_name, round(AVG(rating/position),2)as quality , round(AVG(IF(rating <3,1,0))*100,2)as poor_query_percentage
+from   Queries
+group by query_name
